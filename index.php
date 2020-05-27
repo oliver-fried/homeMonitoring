7b05ec4579c22f6ad70c768b1e8fef864917cccb
Oliver Fried <oliverfried3@gmail.com>
2020-05-27 12:25:52 -0400

    php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <style type="text/css">
+    
+        body { background: black !important; } /* Adding !important forces the browser to overwrite the default style applied by Bootstrap */
+     </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  </head>
+  <div class="row">
+    <div class="col-sm-2">
+    <div class="card text-white bg-dark mb-4 mt-4 ml-4" style="max-width: 18rem;">
+    <div class="card-header">Current Time</div>
+    <div class="card-body">
+      <h5 class="card-title">10:18pm</h5>
+      <p class="card-text">Tuesday, May 26th</p>
+    </div>
+    </div>
+    <div class="card text-white bg-dark mb-4 mt-4 ml-4" style="max-width: 18rem;">
+        <div class="card-header">System Status</div>
+        <div class="card-body">
+          <h5 class="card-title text-success">Armed</h5>
+          <p class="card-text">Tap to unarm</p>
+        </div>
+        </div>
+    
+    </div>
+        <div class="col-sm-5">
+            <div class="card text-white bg-dark mb-4 mt-4 ml-1" style="max-width: 18rem;">
+            <div class="card-header">Furnace Temperatire</div>
+            <div class="card-body">
+              <h1 class="card-title text-warning">700 F</h1>
+              <p class="card-text">
+              </p>
+            </div>
+            </div>
+
+    </div>
+    </div>
+        
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
