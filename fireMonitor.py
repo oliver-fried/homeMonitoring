bccfd3c04e9b289fc8c694319d3a999448b3527c
Oliver <oliverfried3@gmail.com>
2021-01-09 21:48:49 -0500

    first commit

--- fireMonitor.py
+++ fireMonitor.py
@@ -27,13 +27,21 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 
 #defining variables from JSON file
-maxTemp = data["maxTempC"]
+maxTemp = ((data["maxTempF"]- 32)*(0.55555555555555))
 maxSlope = data["maxSlopeC"]
 thermSerialNumber = data["furnaceThermometerSerialNumber"]
 
 #getting the therm file
 thermOneFolder = glob.glob(base_dir + thermSerialNumber)[0]
 deviceOne_file = thermOneFolder + '/w1_slave'
+
+#setting up email sending
+sender_email ='greenridgehomemonitoring@gmail.com'
+rec_email = data['receivingEmail']
+password = '1426Greenridge'
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, password)
 
 
 #getting the temp from the output
@@ -52,33 +60,97 @@ def read_thermOnetemp():
         thermOnetemp_c = round(1.8*(float(temp_string) / 1000.0) +32, 2)
         return thermOnetemp_c
 
+statusArray = []
+
 def timeAlarms(oldStatus):
+    GPIO.output(data['alarmPin'], 0)
 
     status = GPIO.input(airIntakeStatusPin)
     #reading in the raw output
 
-    
-    if abs(status-oldStatus) == 1:
+    if status-oldStatus == 1:
         startingTemp = read_thermOnetemp()
         time.sleep(data["fireMonitorAlarmTimeInSeconds"])
         currentTemp = read_thermOnetemp()
 
-        if (currentTemp - startingTemp) < data["minDegreesChangeToNotRaiseAlarm"]:
+        if currentTemp >= startingTemp:
+            print "Furnace is not cooling down!"
+            server.sendmail(sender_email, rec_email, "WARNING! PLENUM IS NOT COOLING DOWN!")
+
             timeEnd = time.time() + data["alarmRunTimeInSeconds"]
-            while time.time() < timeEnd
+            print "----------------------------------------------"
+            print "the  temp recorded on the last iteration was: " + str(startingTemp)
+            print "the temp recorded on this iteration is: " + str(currentTemp)
+            print "air intake status: " + str(status)
+            while time.time() < timeEnd:
                 GPIO.output(data['alarmPin'], 1)
+
+            GPIO.output(data['alarmPin'], 0)
+            statusArray.insert(0, status)
+            if len(statusArray) > 2:
+                del statusArray[-1]
         
         else:
-            print "Furnace is heating as expected."
-            timeAlarms(status)
+            print "----------------------------------------------"
+            print "the  temp recorded on the last iteration was: " + str(startingTemp)
+            print "the temp recorded on this iteration is: " + str(currentTemp)
+            print "air intake status: " + str(status)
+            statusArray.insert(0, status)
+            if len(statusArray) > 2:
+                del statusArray[-1]
+
+
+    elif status-oldStatus == -1:
+        startingTemp = read_thermOnetemp()
+        time.sleep(data["fireMonitorAlarmTimeInSeconds"])
+        currentTemp = read_thermOnetemp()
+
+        if currentTemp <= startingTemp:
+            print "Furnace is not warming up!"
+            server.sendmail(sender_email, rec_email, "Attention! Plenum is not warming up. Fire has most likely died.")
+
+            timeEnd = time.time() + data["alarmRunTimeInSeconds"]
+            print "----------------------------------------------"
+            print "the  temp recorded on the last iteration was: " + str(startingTemp)
+            print "the temp recorded on this iteration is: " + str(currentTemp)
+            print "air intake status: " + str(status)
+            while time.time() < timeEnd:
+                GPIO.output(data['alarmPin'], 1)
+            GPIO.output(data["alarmPin"], 0)
+            statusArray.insert(0, status)
+            if len(statusArray) > 2:
+                del statusArray[-1]
+            
+        else:
+            print "----------------------------------------------"
+            print "the  temp recorded on the last iteration was: " + str(startingTemp)
+            print "the temp recorded on this iteration is: " + str(currentTemp)
+            print "air intake status: " + str(status)
+            statusArray.insert(0, status)
+            if len(statusArray) > 2:
+                del statusArray[-1]
+
 
     else:
-        timeAlarms(status)
-
+        startingTemp = read_thermOnetemp()
+        time.sleep(data["fireMonitorAlarmTimeInSeconds"])
+        currentTemp = read_thermOnetemp()
+        print "No change"
+        print "----------------------------------------------"
+        print "the  temp recorded on the last iteration was: " + str(startingTemp)
+        print "the temp recorded on this iteration is: " + str(currentTemp)
+        print "air intake status: " + str(status)
+        statusArray.insert(0, status)
+        if len(statusArray) > 2:
+            del statusArray[-1]
         
-timeAlarms(GPIO.input(airIntakeStatusPin))
-        
 
-    
+
+
+statusArray.insert(0, GPIO.input(airIntakeStatusPin))
+
+while True:
+    timeAlarms(statusArray[0])
+
 
 GPIO.cleanup()
